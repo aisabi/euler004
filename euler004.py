1d9f13d8a7b648fab620bf81659a039cb01d1b51
Aissa <aissa.abid@laposte.net>
2020-02-06 20:44:38 +0100

    add max palindrome 3-digit*

--- euler004.py
+++ euler004.py
@@ -1,11 +1,13 @@
 def largestPalindromeProduct(n):
 	# calculate palindrome numbers example : The largest palindrome made from the product of
-	#  two 2-digits numbers is 9009 = 91 × 99
+	# two 2-digits numbers is 9009 = 91 × 99
+    # two 3-digits numbers is 906609
     print('please wait during calculation')
     listeA =[]
     listePalindrome = []
     # for a multiplication of 2 numbers with two-digit
     if  n == 2:
+        print('You choose 2-digit calculation')
         for chiffre in range(10,100):
             for nombre in range(1,100):
                 resultat = chiffre * nombre
@@ -22,14 +24,37 @@ def largestPalindromeProduct(n):
                             listePalindrome.append(analyse1)
                     palindromeTwoDigits =set(listePalindrome)
                     palindromeTwoDigits = sorted(palindromeTwoDigits)
-                    laliste = palindromeTwoDigits         
+                    print('List palindrome :',palindromeTwoDigits)
+                    laliste = palindromeTwoDigits # get max value of the list using the max() method     
         print('Palindrome MAX for two 2-digits numbers is :',max(laliste))
 	
-	# for a multiplication of 2 numbers with two-digit
+	# for a multiplication of 2 numbers with three-digit
     elif n == 3:
-    	print('to do')
+        print('You choose 3-digit calculation')
+        for chiffre in range(900,1000):
+            for nombre in range(900,1000):
+                resultat = chiffre * nombre
+                if resultat > 850000:
+                    resultat = str(resultat)
+                    listeA.append(resultat)
+                    liste1 =set(listeA)
+                    # once the data are collected, analyse them to find out any palindrome
+                    for donnee in liste1:
+                        analyse1 = str(donnee[0])+str(donnee[1])+str(donnee[2])+str(donnee[3])+str(donnee[4])+str(donnee[5])
+                        analyse2 = str(donnee[5])+str(donnee[4])+str(donnee[3])+str(donnee[2])+str(donnee[1])+str(donnee[0])
+                        if analyse1 == analyse2:
+                            listePalindrome.append(analyse1)
+                    palindromeTwoDigits =set(listePalindrome)
+                    palindromeTwoDigits = sorted(palindromeTwoDigits)
+                    print('List palindrome :',palindromeTwoDigits)
+                    for item in palindromeTwoDigits: # get max value of the list using a for loop
+                        maxValeur = 0
+                        item = int(item)
+                        if item > maxValeur: maxValeur = item
+                    
+        print('Palindrome MAX for 3-digit numbers is :', maxValeur)
 
     else:
-    	print('Use number 2 or 3 as argument')
+    	print('Use number 2 for 2-digit calculation OR 3 for 3-digit calculation as argument')
 
 largestPalindromeProduct(2)
